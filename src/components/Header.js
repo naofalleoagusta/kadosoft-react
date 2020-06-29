76683229ec5a2fb3622ff5277c37b0bf23b25735
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2020-06-29 18:33:44 +0700

    fix minor bug animation in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React from "react";
 import NavLink from "./NavLink";
 import Collapse from 'react-bootstrap/Collapse';
 import MediaQuery from "react-responsive";
+import {
+    MIN_DESKTOP_SIZE
+} from "../data/constants";
 
 
 class Header extends React.Component {
@@ -37,34 +40,40 @@ class Header extends React.Component {
     render() {
         let path = this.props.location.pathname;
         return (
-            <header className={`${(path !== "/" && path !== "/contact-us") && "header-height header-bg min-header"} header-nav header-dark ${this.state.status !== "top" && "fixed-header"}`}>
-                <div className="fixed-header-bar">
-                    <div className="navbar navbar-main navbar-expand-lg">
-                        <div className="container">
-                            <NavLink href="/" className="px-0">
-                                <img alt="" className="logo-header" src={require('./../assets/img/logo.png')} title="" />
-                            </NavLink>
-                            <button aria-controls="navbar-main-collapse"
-                                onClick={() => this.setState({ open: !this.state.open })}
-                                aria-expanded={this.state.open} aria-label="Toggle navigation" className="navbar-toggler"
-                                aria-controls="navbar-main-collapse" data-toggle="collapse" type="button">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <Collapse in={this.state.open}>
-                                <div className="navbar-collapse navbar-collapse-overlay" id="navbar-main-collapse">
-                                    <ul className="navbar-nav ml-auto" onClick={() => this.setState({ open: !this.state.open })}>
-                                        <li className="nav-item"><NavLink className="nav-link" href="/" >Home</NavLink></li>
-                                        <li className="nav-item"><NavLink className="nav-link" href="/service">Service</NavLink></li>
-                                        <li className="nav-item"><NavLink className="nav-link" href="/catalog">Catalog</NavLink></li>
-                                        <li className="nav-item"><NavLink className="nav-link" href="/about-us">About</NavLink></li>
-                                        <li className="nav-item"><NavLink className="nav-link" href="/contact-us">Contact</NavLink></li>
-                                    </ul>
+
+            <MediaQuery minWidth={MIN_DESKTOP_SIZE}>
+                {isDesktop => {
+                    return (<header className={`${(path !== "/" && path !== "/contact-us") && "header-height header-bg min-header"} header-nav header-dark ${this.state.status !== "top" && "fixed-header"}`}>
+                        <div className="fixed-header-bar">
+                            <div className="navbar navbar-main navbar-expand-lg">
+                                <div className="container">
+                                    <NavLink href="/" className="px-0">
+                                        <img alt="" className="logo-header" src={require('./../assets/img/logo.png')} title="" />
+                                    </NavLink>
+                                    <button aria-controls="navbar-main-collapse"
+                                        onClick={() => this.setState({ open: !this.state.open })}
+                                        aria-expanded={this.state.open} aria-label="Toggle navigation" className="navbar-toggler"
+                                        aria-controls="navbar-main-collapse" data-toggle="collapse" type="button">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                    <Collapse in={this.state.open}>
+                                        <div className="navbar-collapse navbar-collapse-overlay" id="navbar-main-collapse">
+                                            <ul className="navbar-nav ml-auto" onClick={() => !isDesktop && this.setState({ open: !this.state.open })}>
+                                                <li className="nav-item"><NavLink className="nav-link" href="/" >Home</NavLink></li>
+                                                <li className="nav-item"><NavLink className="nav-link" href="/service">Service</NavLink></li>
+                                                <li className="nav-item"><NavLink className="nav-link" href="/catalog">Catalog</NavLink></li>
+                                                <li className="nav-item"><NavLink className="nav-link" href="/about-us">About</NavLink></li>
+                                                <li className="nav-item"><NavLink className="nav-link" href="/contact-us">Contact</NavLink></li>
+                                            </ul>
+                                        </div>
+                                    </Collapse>
                                 </div>
-                            </Collapse>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </header >
+                    </header >
+                    );
+                }}
+            </MediaQuery>
         );
     }
 }
---
+++ src/data/constants.js
@@ -0,0 +1,6 @@
+const MIN_DESKTOP_SIZE = 992;
+
+
+export {
+    MIN_DESKTOP_SIZE
+};
